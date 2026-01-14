dd439b1486aada12cd1e4ffb73b293815d44ff0e
Zhang Lijie <202034028+write-for-bug@users.noreply.github.com>
2026-01-14 16:33:15 +0800

    优化导入

--- bestman/src/bestman/robots/__init__.py
+++ bestman/src/bestman/robots/__init__.py
@@ -2,5 +2,3 @@ from .config import RobotConfig
 from .base_robot import BaseRobot
 from .utils import *
 from .factory import make_robot_from_config
-from . import xarm
-from . import startouch
--- test.py
+++
@@ -1,1 +0,0 @@
-from bestman.robots.startouch import SingleArm
