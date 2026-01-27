0bc86fae778c8fcb693947a2c64c6fdceb1c3c51
Zhang Lijie <202034028+write-for-bug@users.noreply.github.com>
2026-01-27 21:51:10 +0800

    replay注释

--- examples/replay/replay.py
+++ examples/replay/replay.py
@@ -7,10 +7,16 @@ from bestman.utils import select_multi_sessions_dir,select_session_subdir
 from bestman.utils import load_trajectory,rpy2T,transform_traj
 from bestman.robots import make_robot_from_config 
 from bestman.utils import TrajReplayer
+import argparse
+
+# activate can
+# sudo ip link set can0 up type can bitrate 1000000 
+
+
 
 DATA_ROOT='/media/ark/B2D6-285E'
 
-# ==================== Startouch 配置 ====================
+# ==================== 初始化Startouch  ====================
 config = StartouchConfig(
     id="startouch",     # 多臂debug以及logger用                   
     dof=6,                                          
@@ -20,6 +26,9 @@ config = StartouchConfig(
     }
 )
 robot = make_robot_from_config(config)
+
+# ==================== 初始化replayer ====================
+
 replayer = TrajReplayer(robot=robot)
 replayer.load_data(DATA_ROOT)
 
@@ -27,13 +36,13 @@ try:
     robot.connect()      
     robot.go_home()
 
-    pose = robot.get_ee_pose()#[x,y,z,r,p,y](m,rad)
+    #根据home点初始化T矩阵对标传感器零点
+    pose = robot.get_ee_pose()  #[x,y,z,r,p,y](m,rad)
     T_robot_init = rpy2T(pose)
-   
+    #用这个矩阵对UMI数据做一次Transform
     replayer.transform_traj(T_robot_init=T_robot_init)
-    
+    #replay
     replayer.replay(speed_rate=1.0)
-
 
 
 except Exception as e:
